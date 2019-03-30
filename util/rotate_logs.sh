38f48597c299f5617b0a405ff42fee6ac964babb
bootladder <admin@bootladder.com>
2019-03-30 18:54:59 -0400

    add rotate_logs_every_X_seconds.sh and mkdir -p ../logs_raw_backup before rotating

--- util/rotate_logs.sh
+++ util/rotate_logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mv ../logs/access.log ../logs/access.log.$(date +%Y-%m-%d-%s)
+mkdir -p ../logs_raw_backup
+mv ../logs/access.log ../logs_raw_backup/access.log.$(date +%Y-%m-%d-%s)
 touch ../logs/access.log
 docker exec nginx-proxy /util/send_reload_logs_signal.sh
 
---
+++ util/rotate_logs_every_X_seconds.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo ./rotate_logs.sh
+echo rotate logs then sleep for $1 seconds 
+sleep $1
